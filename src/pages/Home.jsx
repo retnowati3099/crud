e0a8ac159032e8cf273a727dd07a674179494c03
Retno Wati <retnowati3099@gmail.com>
2023-09-27 09:16:31 +0700

    added title, button add, and button search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 
 function Home() {
-  return <>
-  {/* Menambahkan title */}
-  {/* Menambahkan add button dan search bar */}
-  {/* Menampilkan table */}
-  </>;
+  return (
+    <>
+      {/* Menambahkan title */}
+      <div className="container-full p-3">
+        <h2 className="text-center">Employee Datas</h2>
+        <div className="d-flex justify-content-between align-items-center">
+          <button className="btn btn-primary">
+            <i className="bi bi-person-add"></i> Add Employee
+          </button>
+          <input
+            type="text"
+            placeholder="Search employee ..."
+            className="p-2"
+          />
+        </div>
+      </div>
+      {/* Menambahkan add button dan search bar */}
+      {/* Menampilkan table */}
+    </>
+  );
 }
 
 export default Home;
